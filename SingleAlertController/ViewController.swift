8fa765d1474d5240d374c5ee29df07e49f5357e8
사용자 방문 <Guest@imac-3.local>
2019-04-10 16:50:50 +0900

    alertController

--- SingleAlertController/ViewController.swift
+++ SingleAlertController/ViewController.swift
@@ -15,6 +15,43 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+    let alertController = UIAlertController(title: "카메라", message: "Take a picture!", preferredStyle: .actionSheet)
+        
+        let camAction = UIAlertAction(title: "Camera", style: .default, handler: nil)
+    
+        let libAction = UIAlertAction(title: "Library", style: .default, handler: nil)
+        
+        let saveAction = UIAlertAction(title: "save", style: .default, handler: nil)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let delAction = UIAlertAction(title: "Delete", style: .default, handler: {(action: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.blue
+            
+        })
 
+        alertController.addAction(camAction)
+        alertController.addAction(cancelAction)
+        alertController.addAction(libAction)
+        alertController.addAction(saveAction)
+        alertController.addAction(delAction)
+        
+        present(alertController, animated: true, completion: nil)
+    
+    }
+    
+    @IBAction func bt2(_ sender: Any) {
+    let alert = UIAlertController(title: "Hey AppCoda", message: "What do you want to do?", preferredStyle: .alert)
+    
+    let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+    
+    alert.addAction(ok)
+    
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
 }
 
